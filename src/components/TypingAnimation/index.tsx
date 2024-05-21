065ad461d4726cdda6fd9e944d1c31f300754658
Dave Schumaker <daveschumaker@users.noreply.github.com>
2024-05-20 23:21:48 -0700

    chore: add typing animation component

---
+++ src/components/TypingAnimation/index.tsx
@@ -0,0 +1,11 @@
+import './typingAnimation.css'
+
+export default function TypingAnimation() {
+  return (
+    <div className="typing">
+      <span className="circle scaling"></span>
+      <span className="circle scaling"></span>
+      <span className="circle scaling"></span>
+    </div>
+  )
+}
